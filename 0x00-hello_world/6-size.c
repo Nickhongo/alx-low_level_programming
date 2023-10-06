0d5406bfb4dc7073c0d0493074d8a0b8bab01a0e
Nickhongo <hongonicholas0@gmail.com>
2023-10-06 04:44:36 -0700

    data types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main-'main' entry point
+ * Return:always a success(0)
+ */
+int main(void)
+{
+	printf("size of char :%lu byte(s)\n", sizeof(char));
+	printf("size of an int :%lu byte(s)\n", sizeof(int));
+	printf("size of a long int :%lu byte(s)\n", sizeof(long));
+	printf("size of a long long int :%lu byte(s)\n", sizeof(long long));
+	printf("size of a float :%lu byte(s)", sizeof(float));
+
+	return (0);
+}
