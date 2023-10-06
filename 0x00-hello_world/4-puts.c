564cd1e8e1400413caf50f511ccdd5415662499d
Nickhongo <hongonicholas0@gmail.com>
2023-10-06 04:04:41 -0700

    hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 /**
- * main - is my entry point
- *main description:'the programs description'
- * return - will be (0) if correct
-*/
+ * main - 'main' entry point
+ * @void:empty parameters
+ * Description:"main" serves aas the entry point of a C program
+ * int main (void):main fuction does not accept any parameters
+ * Return:always a success (0)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	return (0);/*indicates execution was a success*/
 }
