f4f848a1630853f206d12f8d443d208627ce0f34
Nickhongo <hongonicholas0@gmail.com>
2023-10-06 04:02:05 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 /**
- * main - it is the 'main' entry point
- * void - it is an empty parameter list
- * Description -"main" entry point
- * int main (void):accepts no parameters
- * Return : always 0 (success)
+ * main - 'main' entry point
+ * @void:empty parameters
+ * Description:"main" serves aas the entry point of a C program
+ * int main (void):main fuction does not accept any parameters
+ * Return:always a success (0)
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	/* Return always 0 (Success)*/
-	return (0);
+	return (0);/*indicates succesful execution*/
 }
