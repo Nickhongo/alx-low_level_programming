8fecb7fd089262b2756984005c022a50bf2fc7ab
Nickhongo <hongonicholas0@gmail.com>
2023-10-06 19:23:31 -0700

    positive/negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,15 +2,14 @@
 #include <time.h>
 /* more headers goes there */
 #include <stdio.h>
+
+/* betty style doc for function main goes there */
 /**
  * main -'main' entry point
  * Description:"main" serves as the entry point for the C program
  * int main(void): main function doesnt accept any parameters
  *Return:always 0 (success)
  */
-
-/* betty style doc for function main goes there */
-/*for the program. It initializes variables, performs necessary setup, and executes the core logic of the program*/
 int main(void)
 {
 	int n;
@@ -20,15 +19,15 @@ int main(void)
 	/* your code goes there */
 	if (n > 0)
 	{
-		printf("n is positive\n");
+		printf("%d is positive\n");
 	}
 	else if (n == 0)
 	{
-		printf("n is zero\n");
+		printf("%d is zero\n");
 	}
-	else
+	else if
 	{
-		printf("n is negative\n");
+		printf("%d is negative\n");
 	}
 	return (0);
 }
