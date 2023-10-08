7c18398fd4436ed56dae11301f62ed7a47fb09a4
Nickhongo <hongonicholas0@gmail.com>
2023-10-08 23:32:20 +0300

    102 print

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/**
+ * main -'main' entry point
+ * @void:empty parameter list
+ * Description:"main" serves as an entry point to a C program
+ * int main(void):main function doesnt accept any parameters
+ * Return:always 0 (success)
+ */
+int main(void)
+{
+	int num1;
+	int num2;
+
+	for (num1 = 0; num1 <= 99; num1++)
+	{
+		for (num2 = num1; num2 <= 99; num2++)
+		{
+			putchar((num1 / 10) + '0');
+			putchar((num1 % 10) + '0');
+
+			putchar(' ');
+
+			putchar((num2 / 10) + '0');
+			putchar((num2 % 10) + '0');
+
+			if (num1 != 99 || num2 != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
