946ec1f7921cda0faca1a6920148df9a203a916b
Nickhongo <hongonicholas0@gmail.com>
2023-10-08 23:54:54 +0300

    different combinations of two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -14,24 +14,21 @@ int main(void)
 
 	for (num1 = 0; num1 <= 8; num1++)
 	{
-
 		for (num2 = num1 + 1; num2 <= 9; num2++)
+		{
+			putchar(num1 + '0');
+			putchar(num2 + '0');
 
-	{
-		putchar(num1 + '0');
-		putchar(num2 + '0');
-
-
-		if (num1 != 8 || num2 != 9)
-	{
-
-		putchar(',');
-	}	putchar(' ');
-
+			if (num1 != 8 || num2 != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
 	}
 
-	}
 	putchar('\n');
+
 	return (0);
 }
 
