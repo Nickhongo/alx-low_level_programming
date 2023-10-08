57df02da598bbd39314b35f397774e3362063d71
Nickhongo <hongonicholas0@gmail.com>
2023-10-08 07:58:57 +0300

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - 'main' is the entry point
+ * @void: has no parameters
+ * Description: "main" serves as an entry point to the program C
+ * int main(void): serves no parameters
+ * Return: always 0(success)
+ */
+int main(void)
+{
+	int number;
+
+	for (number = 0 ; number <= 9 ; number++)
+	{
+		printf("%d ", number);
+	}
+
+	printf("\n");
+
+	return (0);
+
+}
