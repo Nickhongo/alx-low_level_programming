d5b50dd92f0d5db43403138ae89cdc1aca6fd629
Nickhongo <hongonicholas0@gmail.com>
2023-10-09 23:27:56 +0300

    standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,12 @@
-#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - the entry point of the program
+ * Description: printing a woord
+ * Return: returns 1 if successful
+ */
 int main(void)
 {
-	char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-	write(STDERR_FILENO, message, 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
 	return (1);
 }
