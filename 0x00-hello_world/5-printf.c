79ab184a4d68f0b827c56e5892c0fec8327130b6
Nickhongo <hongonicholas0@gmail.com>
2023-10-09 23:08:51 +0300

    intel

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,2 @@
-#include <stdio.h>
-/**
- * main - 'main' entry point
- * @void:empty parameters
- * Description:"main" serves aas the entry point of a C program
- * int main (void):main fuction does not accept any parameters
- * Return:always a success (0)
- */
-int main(void)
-{
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);/*indicates succesful execution*/
-}
+#!/bin/bash
+gcc -S -masm=intel $CFILE
