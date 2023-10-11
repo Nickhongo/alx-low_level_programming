8eb64d153bcb096f258a043ba281a79a00407b86
Nickhongo <hongonicholas0@gmail.com>
2023-10-11 15:50:24 +0300

    readme file for loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - the entry point of the program
+ * Description: printing _putchar
+ * int main(void) : accepts no parameters
+ * Return: returns 0 if successful
+ */
+
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+}
