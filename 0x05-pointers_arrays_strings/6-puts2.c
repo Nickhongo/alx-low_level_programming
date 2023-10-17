0a04e1fd3812cff2e1952500fcf1d92d8aa4099a
Nickhongo <hongonicholas0@gmail.com>
2023-10-17 16:31:31 +0300

    Half the lies they tell about me aren't true

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -5,19 +5,15 @@
  * @str: reps a string
  * Return: returns 0
  */
-
 void puts2(char *str)
 {
-	int a = 0;
+	int a;
 
-	if (str == NULL)
-	{
-		return;
-	}
+	for (a = 0; str[a] != '\0'; a++)
 
-	for (a = 0; str[a] != '\0'; a += 2)
+	if (a % 2 == 0)
 	{
-		_putchar(str[a]);
+	_putchar(str[a]);
 	}
 	_putchar('\n');
 }
