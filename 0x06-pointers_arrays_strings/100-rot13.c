9be4f158cf2b80b22383f87f72015f4bcb7b2775
Nickhongo <hongonicholas0@gmail.com>
2023-10-25 01:08:21 +0300

    readme file

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,1 +1,26 @@
+#include "main.h"
+/**
+ * rot13 - encodes a string
+ * @str: string encoded
+ * Return: returns a string
+ */
 
+char *rot13(char *str)
+{
+	int i, j;
+	char c[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char d[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; c[j] != '\0'; j++)
+		{
+			if (str[i] == c[j])
+			{
+				str[i] = d[j];
+				break;
+			}
+		}
+	}
+	return (str);
+}
