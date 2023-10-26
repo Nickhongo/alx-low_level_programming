d9d8724d33b2dfb7383423edcf43beec8ad63a28
Nickhongo <hongonicholas0@gmail.com>
2023-10-27 01:22:28 +0300

    . Inception. Is it possible?

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,42 @@
+#include "main.h"
+int check_palindrome(char *s, int i, int len);
+int _strlen_recursion(char *s);
+
+/**
+ * is_palindrome - checks for palindrome
+ * @s: string
+ * Return: returns 1 or 0
+ */
+
+int is_palindrome(char *s)
+{
+	if (*s == 0)
+		return (1);
+	return (check_palindrome(s, 0, _strlen_recursion(s)));
+}
+/**
+ * _strlen_recursion- checks for string recursion
+ * @s: string
+ * Return: returns 0
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
+}
+/**
+ * check_palindrome - checks for palindrome
+ * @s: string
+ * @i:iterator
+ * @len: length of a string
+ * Return: returns 1 or 0
+ */
+int check_palindrome(char *s, int i, int len)
+{
+	if (*(s + i) != *(s + len - 1))
+		return (0);
+	if (i >= len)
+		return (1);
+	return (check_palindrome(s, i + 1, len - 1));
+}
--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,35 +1,20 @@
 #include "main.h"
+#include <string.h>
 /**
  * is_prime_number - checks for prime number
  * check_prime - checks for prime
  * @n: integer checked
- * Return: returns 0 or 1
+ * Return: returns 0
  */
-
 int is_prime_number(int n)
 {
-	if (n <= 1)
-		return (0);
-	return (another_prime(n, n - 1));
-}
-/**
- * another_prime - calculates if a number is prime recur
- * @n: number to evaluate
- * @i: iterator
- *
- * Return: 1 if n is prime, 0 if not
- */
+	int primeNumber = 0;
 
-int another_prime(int n, int i)
-{
 	if (n < 2)
-		return (0);
-	if (n == 2)
-		return (1);
-	if (n == i)
-		return (1);
-	if (n % i == 0)
-		return (0);
-	if (n % i != 0)
-	return (another_prime(n, i - 1));
+		primeNumber = 0;
+	else if (n == 2)
+		primeNumber = 1;
+	else if (n % 2 == 1)
+		primeNumber = 1;
+	return (primeNumber);
 }
