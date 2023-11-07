e7a51ec277ced973d7cb1fe5ebd7667ab7011398
Nickhongo <hongonicholas0@gmail.com>
2023-11-07 11:13:28 +0300

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG
+#define DOG
+#include <stdio.h>
+/**
+ * struct dog - new type definition
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
