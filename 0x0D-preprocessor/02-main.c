11c266ccf2b35132b4f0d42d4d400105e415e066
Nickhongo <hongonicholas0@gmail.com>
2023-11-07 13:18:58 +0300

    File name

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+#include <stdio.h>
+/**
+ * main - print a program
+ * @void: empty parameter
+ * Return: returns 0
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
