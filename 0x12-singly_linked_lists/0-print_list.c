244dc12a455f80b56ae52a0c7b82f38ac7a4433b
Nickhongo <hongonicholas0@gmail.com>
2023-11-17 12:49:40 +0300

    The Hare and the Tortoise

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * print_list - prints all the elements of a list
+ * @h: points the next node
+ *
+ * Description: singly linked lists
+ * Return: print_list
+ */
+size_t print_list(const list_t *h)
+{
+	int i = 0;
+	if (list_t[i] != NULL)
+		return;
+	if (h == NULL)
+		printf ("[0] (nil)");
+}
---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include "lists.h"
+/**
+ * printMainFirst - prints before main fucntion is executed
+ */
+void printMainFirst(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
