71d75229c58928a7e5063aab7410628400a54e5f
Nickhongo <hongonicholas0@gmail.com>
2023-11-21 00:45:04 +0300

    Print List

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,14 +8,12 @@ size_t print_listint(const listint_t *h)
 {
 	size_t new_node = 0;
 
-	while (h == NULL)
+	while (h != NULL)
 	{
 		printf("%d\n", h->n);
+		h = h->next;
 		new_node++;
 	}
-	new_node = h->n;
-       	h= h->next;
-
 	return (new_node);
 }
 
