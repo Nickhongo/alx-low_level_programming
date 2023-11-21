a696b9145b9a8c941bf3b89d0d1004c3285b09a6
Nickhongo <hongonicholas0@gmail.com>
2023-11-22 00:53:30 +0300

    Free

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * free_listint2 - frees a list
+ * @head: start of the list
+ * Return: nothing
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+
+	if (head == NULL || *head == NULL)
+		return;
+
+	while ((*head)->next != NULL)
+	{
+		tmp = (*head)->next; 
+		free(*head);
+		*head = tmp;
+	}
+	free(*head);
+	*head = NULL;
+}
