74025e407c6dc6531762a69e5606c94aa2632cdb
Nickhongo <hongonicholas0@gmail.com>
2023-12-01 17:11:00 +0300

    Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,30 +9,25 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd;
 	ssize_t result;
-	FILE *fp;
 
-	if (filename != NULL)
+	if (filename == NULL)
 		return (-1);
 
-	fp = fopen(filename, "r");
-	if (fp != NULL)
-		fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-	else
-		fd = open(filename, O_WRONLY | O_TRUNC);
-
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
-		return (-1);
-
-	if (text_content == NULL)
 	{
-		fclose(fp);
-		close(fd);
-		return (1);
-	}
-	result = write(fd, text_content, strlen(text_content));
-	if (result == -1)
 		return (-1);
-	fclose(fp);
-	close(fd);
-	return (1);
+	}
+	if (text_content != NULL)
+	{
+		result = write(fd, text_content, strlen(text_content));
+		if (result == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+	if (close(fd) == -1)
+		return (-1);
+	return (-1);
 }
