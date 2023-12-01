5b3c819ed622218701cde2446b69ab665f807e5e
Nickhongo <hongonicholas0@gmail.com>
2023-12-01 17:18:46 +0300

    Under the snow correction

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,7 +11,10 @@ int create_file(const char *filename, char *text_content)
 	ssize_t result;
 
 	if (filename == NULL)
+	{
+		dprintf(2, "Error: NULL filename\n");
 		return (-1);
+	}
 
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
@@ -28,6 +31,8 @@ int create_file(const char *filename, char *text_content)
 		}
 	}
 	if (close(fd) == -1)
+	{
 		return (-1);
-	return (-1);
+	}
+	return (1);
 }
