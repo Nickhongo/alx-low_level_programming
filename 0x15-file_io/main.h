f423ee7b907a47ce87322ba4eaec43f7ac67eece
Nickhongo <hongonicholas0@gmail.com>
2023-12-16 18:28:01 +0300

    asabi

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -21,5 +21,15 @@ void check_err_file(int file_from, int file_to, char *argv[]);
 void print_error(const char *message);
 void print_elf_header_info(Elf64_Ehdr *header);
 int main(int argc, char *argv[]);
+void check_elf(unsigned char *e_ident);
+void print_magic(unsigned char *e_ident);
+void print_class(unsigned char *e_ident);
+void print_data(unsigned char *e_ident);
+void print_version(unsigned char *e_ident);
+void print_abi(unsigned char *e_ident);
+void print_osabi(unsigned char *e_ident);
+void print_type(unsigned int e_type, unsigned char *e_ident);
+void print_entry(unsigned long int e_entry, unsigned char *e_ident);
+void close_elf(int elf);
 
 #endif
