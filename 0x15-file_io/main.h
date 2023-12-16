7490e25138b5069b35da80e5b00240045cf9cbb3
Nickhongo <hongonicholas0@gmail.com>
2023-12-16 13:44:04 +0300

    cp

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -17,7 +17,7 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int _putchar(char c);
-int copy_file(int fd, int fp);
+int copy_file(int fd1, int fd2);
 void print_error(const char *message);
 void print_elf_header(const Elf64_Ehdr *header);
 int main(int argc, char *argv[]);
