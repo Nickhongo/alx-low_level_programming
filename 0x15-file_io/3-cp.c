3e7919a511ab27b34f0d20c0a3ef803c54186888
Nickhongo <hongonicholas0@gmail.com>
2023-12-16 19:03:04 +0300

    hhhh

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,36 +1,38 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
- *check_err_file - checks for errors in an opened file
- *@file_from: source file
- *@file_to: destination file
- *@argv: argument vector
+ * error_file - checks if files can be opened.
+ * @file_from: file_from.
+ * @file_to: file_to.
+ * @argv: arguments vector.
+ * Return: no return.
  */
-void check_err_file(int file_from, int file_to, char *argv[])
+void error_file(int file_from, int file_to, char *argv[])
 {
-	if (file_from == -1)/*after opening the file*/
+	if (file_from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	if (file_to == -1)/*after opening the file*/
+	if (file_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
 }
+
 /**
- *main - copy content of a file from source to destination file
- *@argc: argument count
- *@argv: argument vector
- *
- *Return: 0
+ * main - check the code for ALX students.
+ * @argc: number of arguments.
+ * @argv: arguments vector.
+ * Return: Always 0.
  */
 int main(int argc, char *argv[])
 {
 	int file_from, file_to, err_close;
-	ssize_t chars_count, num_wr;
-	char buff[1024];/*copies 1024 at a go*/
+	ssize_t nchars, nwr;
+	char buf[1024];
 
 	if (argc != 3)
 	{
@@ -40,24 +42,26 @@ int main(int argc, char *argv[])
 
 	file_from = open(argv[1], O_RDONLY);
 	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
-	check_err_file(file_from, file_to, argv);
+	error_file(file_from, file_to, argv);
 
-	chars_count = 1024;
-	while (chars_count == 1024)
+	nchars = 1024;
+	while (nchars == 1024)
 	{
-		chars_count = read(file_from, buff, 1024);
-		if (chars_count == -1)
-			check_err_file(-1, 0, argv);
-		num_wr = write(file_to, buff, chars_count);
-		if (num_wr == -1)
-			check_err_file(0, -1, argv);
+		nchars = read(file_from, buf, 1024);
+		if (nchars == -1)
+			error_file(-1, 0, argv);
+		nwr = write(file_to, buf, nchars);
+		if (nwr == -1)
+			error_file(0, -1, argv);
 	}
+
 	err_close = close(file_from);
 	if (err_close == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
 		exit(100);
 	}
+
 	err_close = close(file_to);
 	if (err_close == -1)
 	{
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -17,7 +17,6 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int _putchar(char c);
-void check_err_file(int file_from, int file_to, char *argv[]);
 void print_error(const char *message);
 void print_elf_header_info(Elf64_Ehdr *header);
 int main(int argc, char *argv[]);
@@ -31,5 +30,5 @@ void print_osabi(unsigned char *e_ident);
 void print_type(unsigned int e_type, unsigned char *e_ident);
 void print_entry(unsigned long int e_entry, unsigned char *e_ident);
 void close_elf(int elf);
-
+void error_file(int file_from, int file_to, char *argv[]);
 #endif
