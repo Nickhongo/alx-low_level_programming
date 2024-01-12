6d1613d524743615503064539bf2e3f52947d296
Nickhongo <hongonicholas0@gmail.com>
2024-01-12 19:27:55 +0300

    Find the largest palindrome made from the product of two 3-digit numbers.

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -8,13 +8,15 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new_node = malloc(sizeof(dlistint_t));
+	dlistint_t *new_node;
 	dlistint_t *current;
 	unsigned int i;
 
+	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
+	{
 		return (NULL);
-
+	}
 	new_node->n = n;
 
 	if (idx == 0)
@@ -49,9 +51,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new_node->next = current->next;
 
 	if (current->next != NULL)
+	{
 		current->next->prev = new_node;
+	}
 	current->next = new_node;
-
 	return (new_node);
 }
-
