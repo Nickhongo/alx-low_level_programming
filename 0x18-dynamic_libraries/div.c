c090d4b9b1bd4ff3a967fa701f1cd1e8b3b26fd2
Nickhongo <hongonicholas0@gmail.com>
2024-01-15 19:41:06 +0300

    Create a dynamic library that contains C functions that can be called from Python

---
+++ 0x18-dynamic_libraries/div.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+/**
+ * divide - divides two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: division
+ */
+int divide(int a, int b)
+{
+	return (a / b);
+}
---
+++ 0x18-dynamic_libraries/mod.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+/**
+ * mod - moderation between two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: moderation
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
