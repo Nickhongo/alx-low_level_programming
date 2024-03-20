036cf64dd5dffd8901d5f3b8f897a9532bf6918b
Nickhongo <hongonicholas0@gmail.com>
2024-03-20 19:26:28 +0300

    fix 5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -5,18 +5,15 @@ def island_perimeter(grid):
     Calculate the perimeter of the island described in the grid.
 
     Args:
-    grid (List[List[int]]): A list of lists of integers representing the grid where:
+    grid (List[List[int]]): A list of lists of integers representing the
+    grid where:
                              - 0 represents a water zone
                              - 1 represents a land zone
 
     Returns:
     int: The perimeter of the island.
 
-    Constraints:
-    - Grid is rectangular, with width and height not exceeding 100.
-    - Grid is completely surrounded by water, and there is one island (or nothing).
-    - The island doesn’t have “lakes” (water inside that isn’t connected to the water around the island).
-    """
+     """
     perimeter = 0
     for i in range(len(grid)):
         for j in range(len(grid[0])):
